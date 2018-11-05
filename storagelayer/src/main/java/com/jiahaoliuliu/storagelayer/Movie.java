73161e7273256e620416a18527a6742df8e74397
Jiahao Liu <jiahaoliuliu@gmail.com>
2018-11-05 21:50:35 +0400

    Created the base entity

--- storagelayer/src/main/java/com/jiahaoliuliu/storagelayer/Movie.java
+++ storagelayer/src/main/java/com/jiahaoliuliu/storagelayer/Movie.java
@@ -1,29 +1,72 @@
 package com.jiahaoliuliu.storagelayer;
 
 import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
 
 import com.jiahaoliuliu.domain.IMovie;
 
 @Entity
 public class Movie implements IMovie {
 
+    @PrimaryKey(autoGenerate = true)
+    private String id;
+
+    private String title;
+
+    private String description;
+
+    private String imageUrl;
+
+    public Movie(String id, String title, String description, String imageUrl) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.imageUrl = imageUrl;
+    }
+
     @Override
     public String getId() {
-        return null;
+        return id;
     }
 
     @Override
     public String getTitle() {
-        return null;
+        return title;
     }
 
     @Override
     public String getDescription() {
-        return null;
+        return description;
     }
 
     @Override
     public String getImageUrl() {
-        return null;
+        return ImagesBaseUrl + imageUrl;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
     }
 }
