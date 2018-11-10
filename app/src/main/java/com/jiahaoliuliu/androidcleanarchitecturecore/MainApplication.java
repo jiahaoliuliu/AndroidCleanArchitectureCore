6ebe0c3383508c15b3bfe2f362d2eee9175907a6
Jiahao Liu <jiahaoliuliu@gmail.com>
2018-11-10 17:11:28 +0400

    Using the application module instead of getting the application directly

--- app/src/main/java/com/jiahaoliuliu/androidcleanarchitecturecore/MainApplication.java
+++ app/src/main/java/com/jiahaoliuliu/androidcleanarchitecturecore/MainApplication.java
@@ -4,20 +4,14 @@ import android.app.Application;
 
 public class MainApplication extends Application {
 
-    private static Application application;
     private static MainComponent mainComponent;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        application = this;
         mainComponent = DaggerMainComponent.builder()
-//                .applicationModule(new ApplicationModule(this))
-                .build();
-    }
-
-    public static Application getApplication() {
-        return application;
+            .applicationModule(new ApplicationModule(this))
+            .build();
     }
 
     public static MainComponent getMainComponent() {
